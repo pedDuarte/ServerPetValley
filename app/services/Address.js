c9aaf9bd595105bd797347699c1ed3ad9971ce3f
cristovaoolegario <cristovaoolegario@gmail.com>
2017-10-18 08:22:42 -0200

    Adição do CRUD de Address para testes com POSTMAN.

--- app/services/Address.js
+++ app/services/Address.js
@@ -8,12 +8,21 @@ var Address = {
     },
 
     getAddressById : function(id, callback){
-        return connection.query('SELECT * FROM Address WHERE ID_ADDRESS = ?', [Id], callback);
+        return connection.query('SELECT * FROM Address WHERE ID_ADDRESS = ?', [id], callback);
     },
 
     addAddress : function(address, callback){
         return connection.query('INSERT INTO Address (POSTAL_CODE, LOCATION, TYPE_LOCATION, NEIGHBORHOOD, CITY, STATE, COMPLEMENT, NUMBER_HOUSE) values (?,?,?,?,?,?,?,?)'
         ,[address.postal_code, address.location, address.type_location, address.neighborhood, address.city, address.state, address.complement, address.number_house], callback);
+    },
+
+    updateAddress : function(address, callback){
+        return connection.query('UPDATE Address SET (POSTAL_CODE = ?, LOCATION = ?, TYPE_LOCATION = ?, NEIGHBORHOOD = ?, CITY = ?, STATE = ?, COMPLEMENT = ?, NUMBER_HOUSE = ?) WHERE ID_ADDRESS = ?'
+        ,[address.postal_code, address.location, address.type_location, address.neighborhood, address.city, address.state, address.complement, address.number_house, address.id_address], callback);
+    },
+
+    removeAddress : function(id, callback){
+        return connection.query('DELETE FROM Address WHERE ID_ADDRESS = ?', [id], callback);
     },
 
     getLastAddressInserted : function (callback){
