4336797f7d5bdb6735a20414fcd9454aed5fd942
cristovaoolegario <cristovaoolegario@gmail.com>
2017-10-19 10:06:51 -0200

    Adição do CRUD de TypeSupply para testes com POSTMAN.

--- app/services/TypeSupply.js
+++ app/services/TypeSupply.js
@@ -3,12 +3,12 @@ var connection = dbConnection();
 
 var TypeSupply = {
     
-        getAllTypeSupply : function(callback){
+        getTypeSupply : function(callback){
             return connection.query("SELECT * FROM Type_supply", callback);
         },
     
         getTypeSupplyById : function(id, callback){
-            return connection.query("SELECT * FROM Type_supply WHERE Id = ?", [id], callback);
+            return connection.query("SELECT * FROM Type_supply WHERE ID_TYPE_SUPPLY = ?", [id], callback);
         },
 
         getTypeSupplyByName : function(name, callback){
@@ -20,12 +20,13 @@ var TypeSupply = {
             [type_supply.name_supply, type_supply.unit], callback);
         },
     
-        updateTypeSupply : function(id, type_supply, callback){
-    
+        updateTypeSupply : function(type_supply, callback){
+            return connection.query("UPDATE TYPE_SUPPLY SET(NAME_SUPPLY = ?, UNIT = ?) WHERE ID_TYPE_SUPPLY = ?",
+            [type_supply.name_supply, type_supply.unit, type_supply.id], callback);    
         },
     
         removeTypeSupply : function(id, callback){
-            return connection.query("DELETE FROM Type_supply WHERE Id = ?", [id], callback);
+            return connection.query("DELETE FROM Type_supply WHERE ID_TYPE_SUPPLY = ?", [id], callback);
         }    
     };
     
