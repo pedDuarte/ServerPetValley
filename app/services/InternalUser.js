7ce3ed3c9b4dd72683f3b01984043005be3e0b13
cristovaoolegario <cristovaoolegario@gmail.com>
2017-10-19 10:09:02 -0200

    Adição do CRUD de InternalUser para testes com POSTMAN.

---
+++ app/services/InternalUser.js
@@ -0,0 +1,29 @@
+var dbConnection = require("./../../config/dbConnection");
+var connection = dbConnection();
+
+var InternalUser = {
+    
+    getInternalUsers : function(callback){
+        return connection.query('SELECT * FROM INTERNAL_USER', callback);
+    },
+
+    getInternalUserById : function(id, callback){
+        return connection.query('SELECT * FROM INTERNAL_USER WHERE ID_USER_FK = ?', [id], callback);
+    },
+
+    addInternalUser : function(internalUser, callback){
+        return connection.query('INSERT INTO INTERNAL_USER (POST, ADMISSION_DATE, SHIFT, IS_VOLUNTEER, WORK_DAYS, DEMISSION_DATE, CRMV, CRMV_UF, ID_USER_FK, ID_USER_GROUPS_FK) VALUES(?,?,?,?,?,?,?,?,?,?)', 
+        [internalUser.post, internalUser.admission_date, internalUser.shift, internalUser.is_volunteer, internalUser.work_days, internalUser.demission_date, internalUser.crmv, internalUser.crmv_uf, internalUser.id_user_fk, internalUser.id_user_groups_fk], callback);
+    },   
+
+    updateInternalUser : function(id, user, callback){
+        return connection.query('UPDATE INTERNAL_USER SET(POST = ?, ADMISSION_DATE = ?, SHIFT = ?, IS_VOLUNTEER = ?, WORK_DAYS = ?, DEMISSION_DATE = ?, CRMV = ?, CRMV_UF = ?, ID_USER_GROUPS_FK = ?) WHERE ID_USER_FK = ?', 
+        [internalUser.post, internalUser.admission_date, internalUser.shift, internalUser.is_volunteer, internalUser.work_days, internalUser.demission_date, internalUser.crmv, internalUser.crmv_uf, internalUser.id_user_groups_fk, internalUser.id_user_fk], callback);
+    },
+
+    removeInternalUser : function(id, callback){
+        return connection.query('DELETE FROM INTERNAL_USER WHERE ID_USER_FK = ?', [id], callback);
+    }
+}
+
+module.exports = InternalUser;
