b8c0f3aca26565c6c576665fba1b1e9797df48a6
cristovaoolegario <cristovaoolegario@gmail.com>
2017-10-19 10:07:24 -0200

    Adição do CRUD de Supply para testes com POSTMAN.

---
+++ app/services/Supply.js
@@ -0,0 +1,30 @@
+var dbConnection = require("./../../config/dbConnection");
+var connection = dbConnection();
+
+var Supply = {
+
+    getSupplies : function(callback){
+        return connection.query("SELECT * FROM SUPPLY", callback);
+    },
+
+    getSupplyById : function(id, callback){
+        return connection.query("SELECT * FROM SUPPLY WHERE ID_SUPPLY = ?", [id], callback);
+    },
+
+    addSupply : function(supply, callback){
+        return connection.query("INSERT INTO SUPPLY (DESCRIPTION, EXPIRATION_DATE, ENTRY_DATE, QUANTITY, UPDATE_DATE, ID_USER_FK, ID_TYPE_SUPPLY) VALUES(?,?,?,?,?,?,?)"
+        ,[supply.description, supply.expiration_date, supply.entry_date, supply.quantity, supply.update_date, supply.id_user_fk, supply.id_typesupply],callback);
+    },
+
+    updateSupply : function(supply, callback){
+        return connection.query('UPDATE SUPPLY SET(DESCRIPTION = ?, EXPIRATION_DATE = ?, ENTRY_DATE = ?, QUANTITY = ?, UPDATE_DATE = ?, ID_USER_FK = ?, ID_TYPE_SUPPLY = ?) WHERE ID_SUPPLY = ?',
+         [supply.description, supply.expiration_date, supply.entry_date, supply.quantity, supply.update_date, supply.id_user_fk, supply.id_type_supply, supply.id_supply], callback)
+    },
+
+    removeSupply : function(id, callback){
+        return connection.query("DELETE FROM SUPPLY WHERE ID_SUPPLY = ?", [id], callback);
+    }
+
+};
+
+module.exports = Supply;
