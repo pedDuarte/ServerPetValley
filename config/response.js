84d81f8fcb3532b7e7e151b13b2cd1c3780e2b37
cristovaoolegario <cristovaoolegario@gmail.com>
2017-11-03 21:20:46 -0200

    Alteração dos atributos da response para boolean.

--- config/response.js
+++ config/response.js
@@ -2,13 +2,13 @@ var response = { "sucess" : "false", "details": ""};
 
 var resp = {
     onResult : function(result){
-        response.sucess = "true";
+        response.sucess = true;
         response.details = result;
         return(response);
     },
 
     onError : function(error){
-        response.sucess = "false";
+        response.sucess = false;
         response.details = error;
         return(response);
     }
