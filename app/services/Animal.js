4ea6d82301dbac008b0b49335e1d532e6bd11ba4
cristovaoolegario <cristovaoolegario@gmail.com>
2017-11-03 20:30:26 -0200

    Update no serviço de animal.

--- app/services/Animal.js
+++ app/services/Animal.js
@@ -4,11 +4,11 @@ var connection = dbConnection();
 var Animal = {
 
     getAnimals : function(callback){
-        return connection.query('SELECT * FROM ANIMAL', callback);
+        return connection.query('SELECT id_animal, size, coat, age, neutered, vermifuges, name, description, species FROM ANIMAL', callback);
     },
 
     getAnimalById : function(id, callback){
-        return connection.query('SELECT * FROM ANIMAL WHERE ID_ANIMAL = ?',[id], callback);
+        return connection.query('SELECT id_animal, size, coat, age, neutered, vermifuges, name, description, species FROM ANIMAL WHERE ID_ANIMAL = ?',[id], callback);
     },
 
     addAnimal : function(animal, callback){
@@ -16,10 +16,10 @@ var Animal = {
         [animal.size, animal.coat, animal.age, animal.neutered, animal.vermifuges, animal.name, animal.description, animal.species], callback);
     },
 
-    /*updateAnimal : function(animal, callback){
-        return connection.query('UPDATE ANIMAL SET(SIZE = ?, COAT = ?, AGE = ?, NEUTERED = ?, BITHDATE = ?, VERMIFUGES = ?) WHERE ID_ANIMAL = ?',
+    updateAnimal : function(animal, callback){
+        return connection.query('UPDATE ANIMAL SET(SIZE = ?, COAT = ?, AGE = ?, NEUTERED = ?, VERMIFUGES = ?, NAME = ?, DESCRIPTION = ?, SPECIES = ?) WHERE ID_ANIMAL = ?',
         [animal.size, animal.coat, animal.age, animal.neutered, animal.birthdate, animal.vermifuges, animal.id_animal], callback);
-    },*/
+    },
 
     removeAnimal : function(id, callback){
         return connection.query('DELETE FROM ANIMAL WHERE ID_ANIMAL = ?', [id], callback);
