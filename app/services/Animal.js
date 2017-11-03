1fa87c80f015a4dbb4d371bcf9b20006a3c11709
cristovaoolegario <cristovaoolegario@gmail.com>
2017-11-03 21:12:31 -0200

    Adição da coluna SEX no modelo de animal.

--- app/services/Animal.js
+++ app/services/Animal.js
@@ -4,21 +4,21 @@ var connection = dbConnection();
 var Animal = {
 
     getAnimals : function(callback){
-        return connection.query('SELECT id_animal, size, coat, age, neutered, vermifuges, name, description, species FROM ANIMAL', callback);
+        return connection.query('SELECT id_animal, size, coat, age, neutered, vermifuges, name, description, species, sex FROM ANIMAL', callback);
     },
 
     getAnimalById : function(id, callback){
-        return connection.query('SELECT id_animal, size, coat, age, neutered, vermifuges, name, description, species FROM ANIMAL WHERE ID_ANIMAL = ?',[id], callback);
+        return connection.query('SELECT id_animal, size, coat, age, neutered, vermifuges, name, description, species, sex FROM ANIMAL WHERE ID_ANIMAL = ?',[id], callback);
     },
 
     addAnimal : function(animal, callback){
-        return connection.query('INSERT INTO ANIMAL(SIZE, COAT, AGE, NEUTERED, VERMIFUGES, NAME, DESCRIPTION, SPECIES)  VALUES(?,?,?,?,?,?,?,?)',
-        [animal.size, animal.coat, animal.age, animal.neutered, animal.vermifuges, animal.name, animal.description, animal.species], callback);
+        return connection.query('INSERT INTO ANIMAL(SIZE, COAT, AGE, NEUTERED, VERMIFUGES, NAME, DESCRIPTION, SPECIES, SEX)  VALUES(?,?,?,?,?,?,?,?,?)',
+        [animal.size, animal.coat, animal.age, animal.neutered, animal.vermifuges, animal.name, animal.description, animal.species, animal.sex], callback);
     },
 
     updateAnimal : function(animal, callback){
-        return connection.query('UPDATE ANIMAL SET(SIZE = ?, COAT = ?, AGE = ?, NEUTERED = ?, VERMIFUGES = ?, NAME = ?, DESCRIPTION = ?, SPECIES = ?) WHERE ID_ANIMAL = ?',
-        [animal.size, animal.coat, animal.age, animal.neutered, animal.birthdate, animal.vermifuges, animal.id_animal], callback);
+        return connection.query('UPDATE ANIMAL SET(SIZE = ?, COAT = ?, AGE = ?, NEUTERED = ?, VERMIFUGES = ?, NAME = ?, DESCRIPTION = ?, SPECIES = ?, SEX = ?) WHERE ID_ANIMAL = ?',
+        [animal.size, animal.coat, animal.age, animal.neutered, animal.vermifuges, animal.name, animal.description, animal.species, animal.sex, animal.id_animal], callback);
     },
 
     removeAnimal : function(id, callback){
