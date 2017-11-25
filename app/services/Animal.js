0a571b04a9d6a1f5ef2b2a405bd7788722da53e8
cristovaoolegario <cristovaoolegario@gmail.com>
2017-11-25 01:14:06 -0200

    Adição da funcionalidade de imagens em animal.

--- app/services/Animal.js
+++ app/services/Animal.js
@@ -1,19 +1,24 @@
 var dbConnection = require("./../../config/dbConnection");
 var connection = dbConnection();
+var fs = require("fs");
 
 var Animal = {
 
     getAnimals : function(callback){
-        return connection.query('SELECT id_animal, size, coat, age, neutered, vermifuges, name, description, species, sex FROM ANIMAL', callback);
+        return connection.query('SELECT id_animal, size, coat, age, neutered, vermifuges, name, description, species, sex, avatar FROM ANIMAL', callback);
     },
 
     getAnimalById : function(id, callback){
-        return connection.query('SELECT id_animal, size, coat, age, neutered, vermifuges, name, description, species, sex FROM ANIMAL WHERE ID_ANIMAL = ?',[id], callback);
+        return connection.query('SELECT id_animal, size, coat, age, neutered, vermifuges, name, description, species, sex, avatar  FROM ANIMAL WHERE ID_ANIMAL = ?',[id], callback);
     },
 
     addAnimal : function(animal, callback){
-        return connection.query('INSERT INTO ANIMAL(SIZE, COAT, AGE, NEUTERED, VERMIFUGES, NAME, DESCRIPTION, SPECIES, SEX)  VALUES(?,?,?,?,?,?,?,?,?)',
-        [animal.size, animal.coat, animal.age, animal.neutered, animal.vermifuges, animal.name, animal.description, animal.species, animal.sex], callback);
+        var bitmap = new Buffer(animal.avatar, 'base64');
+        var image_path = "public/images/pet/"+animal.name+Date.now()+".jpg";
+        fs.writeFileSync(image_path, bitmap);
+
+        return connection.query('INSERT INTO ANIMAL(SIZE, COAT, AGE, NEUTERED, VERMIFUGES, NAME, DESCRIPTION, SPECIES, SEX, AVATAR)  VALUES(?,?,?,?,?,?,?,?,?,?)',
+        [animal.size, animal.coat, animal.age, animal.neutered, animal.vermifuges, animal.name, animal.description, animal.species, animal.sex, image_path], callback);
     },
 
     updateAnimal : function(animal, callback){
