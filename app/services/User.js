84ccee200767674bf8f714fa73978d73c2b8c925
cristovaoolegario <cristovaoolegario@gmail.com>
2017-12-02 02:33:06 -0200

    Correção de endereço em User.

--- app/services/User.js
+++ app/services/User.js
@@ -34,8 +34,8 @@ var User = {
     },   
 
     updateUser : function(user, callback){
-        return connection.query("UPDATE USER SET CODE_CPF = ?, NAME = ?, SURNAME = ?, BIRTHDATE = ?, SEX = ?, EMAIL = ?, PASSWORD = ?, CREATE_TIME = ?, CELLPHONE = ?, PHONE_NUMBER = ?, ID_ADDRESS_FK = ? WHERE ID_USER = ?;"
-        ,[user.code_cpf,user.name, user.surname, user.birthdate, user.sex, user.email, user.password, user.create_time, user.cellphone, user.phone_number, user.id_address_fk, user.id], callback)
+        return connection.query("UPDATE USER SET CODE_CPF = ?, NAME = ?, SURNAME = ?, BIRTHDATE = ?, SEX = ?, EMAIL = ?, PASSWORD = ?, CREATE_TIME = ?, CELLPHONE = ?, PHONE_NUMBER = ? WHERE ID_USER = ?;"
+        ,[user.code_cpf,user.name, user.surname, user.birthdate, user.sex, user.email, user.password, user.create_time, user.cellphone, user.phone_number, user.id], callback)
     },
 
     removeUser : function(id, callback){
