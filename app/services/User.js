99766ffe8c483875dd5eaf7be917bb6cee8d595e
cristovaoolegario <cristovaoolegario@gmail.com>
2017-12-02 01:58:09 -0200

    Correção do id do where em user.

--- app/services/User.js
+++ app/services/User.js
@@ -35,7 +35,7 @@ var User = {
 
     updateUser : function(user, callback){
         return connection.query("UPDATE USER SET CODE_CPF = ?, NAME = ?, SURNAME = ?, BIRTHDATE = ?, SEX = ?, EMAIL = ?, PASSWORD = ?, CREATE_TIME = ?, CELLPHONE = ?, PHONE_NUMBER = ?, ID_ADDRESS_FK = ? WHERE ID_USER = ?;"
-        ,[user.code_cpf,user.name, user.surname, user.birthdate, user.sex, user.email, user.password, user.create_time, user.cellphone, user.phone_number, user.id_address_fk, user.id_user], callback)
+        ,[user.code_cpf,user.name, user.surname, user.birthdate, user.sex, user.email, user.password, user.create_time, user.cellphone, user.phone_number, user.id_address_fk, user.id], callback)
     },
 
     removeUser : function(id, callback){
